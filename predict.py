892aea6f79ce906ee9b2c68001acf76b9a46579f
osawa-koki <koki@osawa.dev>
2024-10-22 15:30:56 +0900

    画像URLからも認識できるように。

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import sys
+import urllib.request
 
 from keras.models import load_model
 import numpy as np
@@ -9,8 +10,17 @@ image_size = 50
 
 
 def main():
-    image_path = sys.argv[1]
-    image = Image.open(image_path)
+    argv = sys.argv
+    if len(argv) < 2:
+        i = input("Please provide an image URL: ")
+        argv.append(i)
+    url = argv[1]
+    if url.startswith("data:image"):
+        raise ValueError("Please provide a valid image URL")
+    if url.startswith("http"):
+        urllib.request.urlretrieve(url, "image.jpeg")
+        print("Downloaded image from URL")
+    image = Image.open("image.jpeg")
     image = image.convert('RGB')
     image = image.resize((image_size, image_size))
     data = np.asarray(image) / 255
